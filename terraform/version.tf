b3846e06d8b2459fab42cdee394e2cd082eadee1
SIFRIZZ <rizwantausif@outlook.com>
2025-04-16 17:45:07 +0400

    created vpc

--- terraform/version.tf
+++ terraform/version.tf
@@ -1,16 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.94.1"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 
   default_tags {
     owner = "sifrizz"
   }
+}
+variable "region" {
+  description = "aws region"
+  default     = "us-east-1"
 }
